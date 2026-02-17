f89907c59c26f1ebb5c2b025e2b459fc616e621c
Marc Portabella <portabella.marc@gmail.com>
2026-02-17 16:00:01 +0100

    Cleanup

--- composeApp/src/webMain/kotlin/com/marcportabella/immichuploader/web/PromiseInterop.kt
+++
@@ -1,8 +0,0 @@
-package com.marcportabella.immichuploader.web
-
-import kotlinx.coroutines.await
-import kotlin.js.JsAny
-import kotlin.js.Promise
-
-@OptIn(ExperimentalWasmJsInterop::class)
-suspend fun <T> Promise<JsAny?>.awaitJs(): T = await()
